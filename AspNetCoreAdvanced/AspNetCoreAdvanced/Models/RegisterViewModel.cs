39a66f7083c51f0d6632d9d6ed849d3ec3d0121b
Venkata Ramana Gollakota <gramana30@gmail.com>
2019-12-21 20:04:49 +0530

    Login and Register pages

---
+++ AspNetCoreAdvanced/AspNetCoreAdvanced/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAdvanced.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Passwords does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
